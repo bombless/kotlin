36fb9343c036181eaaa789685aa5a441b34159ad
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2018-08-29 18:23:40 +0200

    Fix KType.javaType for suspend function return type
     #KT-26293 Fixed

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/KCallableImpl.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/KCallableImpl.kt
@@ -17,10 +17,14 @@
 package kotlin.reflect.jvm.internal
 
 import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.load.java.descriptors.JavaCallableMemberDescriptor
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
+import java.lang.reflect.WildcardType
 import java.util.*
+import kotlin.coroutines.experimental.Continuation
 import kotlin.reflect.*
 import kotlin.reflect.jvm.javaType
 
@@ -73,7 +77,9 @@ internal abstract class KCallableImpl<out R> : KCallable<R> {
         get() = _parameters()
 
     private val _returnType = ReflectProperties.lazySoft {
-        KTypeImpl(descriptor.returnType!!) { caller.returnType }
+        KTypeImpl(descriptor.returnType!!) {
+            extractContinuationArgument() ?: caller.returnType
+        }
     }
 
     override val returnType: KType
@@ -197,4 +203,19 @@ internal abstract class KCallableImpl<out R> : KCallable<R> {
                 else -> throw UnsupportedOperationException("Unknown primitive: $type")
             }
         } else null
+
+    private fun extractContinuationArgument(): Type? {
+        if ((descriptor as? FunctionDescriptor)?.isSuspend == true) {
+            // kotlin.coroutines.Continuation<? super java.lang.String>
+            val continuationType = caller.parameterTypes.lastOrNull() as? ParameterizedType
+            if (continuationType?.rawType == Continuation::class.java) {
+                // ? super java.lang.String
+                val wildcard = continuationType.actualTypeArguments.single() as? WildcardType
+                // java.lang.String
+                return wildcard?.lowerBounds?.first()
+            }
+        }
+
+        return null
+    }
 }
--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
@@ -17754,6 +17754,11 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
                     runTest("compiler/testData/codegen/box/reflection/mapping/types/supertypes.kt");
                 }
 
+                @TestMetadata("suspendFun.kt")
+                public void testSuspendFun() throws Exception {
+                    runTest("compiler/testData/codegen/box/reflection/mapping/types/suspendFun.kt");
+                }
+
                 @TestMetadata("topLevelFunctions.kt")
                 public void testTopLevelFunctions() throws Exception {
                     runTest("compiler/testData/codegen/box/reflection/mapping/types/topLevelFunctions.kt");
